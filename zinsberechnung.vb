9a3eb7a97c4c048944e7f20e68ceb66959049e47
ocdaederus <87893295+ocdaederus@users.noreply.github.com>
2022-12-12 21:31:14 +0100

    Update zinsberechnung.vb to version 0.1.4
    - added the calculation of zinsen
    - added the calculation of kumultzinsen and gesamtkapital as a new label object with its own process of calculation
    - added an extra window

--- zinsberechnung.vb
+++ zinsberechnung.vb
@@ -18,13 +18,45 @@ Public Class zinsberechnung
             gesamtkapital = ((kapital * zinssatz) / 100) + kapital.ToString("#,##0.00")
         
             gesamtkapital = Math.Round(gesamtkapital, 2)
+        
+            zinsen = kapital * zinssatz / 100
+        
+            zinsen = Math.Round(zinsen, 2)
 
-            kapital = gesamtkapital.ToString("#,##0.0")
+            kapital = gesamtkapital
 
             'Ausgabe
-            lst_ergebnisse.Items.Add(i & "                      " & zinssatz & "%" & "                      " & gesamtkapital & "€")
+            lst_ergebnisse.Items.Add(i & "                  " & Format("#,##0.00", [zinsen]) & "€" & "                  " & gesamtkapital & "€")
 
         Next i
+
+    End Sub
+
+        Private Sub cmd_kontrolle_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmd_kontrolle.Click
+
+        'Eingabe
+
+        kapital = txt_kapital.Text
+        zinssatz = txt_zinssatz.Text
+        zeitraum = txt_zeitraum.Text
+
+        'Verarbeitung
+
+        kumultzinsen = kapital * ((1 + zinssatz / 100) ^ zeitraum) - kapital
+
+        kumultzinsen = Math.Round(kumultzinsen, 2)
+
+        'Ausgabe
+
+        lbl_kumultzinsen.Text = kumultzinsen.ToString("#,##0.00€")
+
+        lbl_gesamtkapital.Text = gesamtkapital.ToString("#,##0.00€")
+
+        If kapital <= 0 And zeitraum <= 0 And (zinssatz = 0 Or zinssatz < 0) Then
+
+            MsgBox("Bitte geben Sie Ihr Kapital, den Zinssatz sowie den Zeitraum an und lassen Sie das am Ende zu erhaltene Geld berechnen, damit die Kontrolle erfolgen kann")
+
+        End If
 
     End Sub
 
@@ -34,14 +66,22 @@ Public Class zinsberechnung
         txt_zeitraum.Clear()
         txt_kapital.Clear()
         lst_ergebnisse.Items.Clear()
+        lbl_kumultzinsen.Text = ""
+        lbl_gesamtkapital.Text = ""
 
     End Sub
 
     Private Sub cmd_beenden_Click(sender As Object, e As System.EventArgs) Handles cmd_beenden.Click
 
-        'Application.Exit()
-        Me.Close()
+        Application.Exit()
+        'Me.Close()
         End
+
+    End Sub
+
+    Private Sub cmd_zusatz_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles cmd_zusatz.Click
+
+        zusatz.Show()
 
     End Sub
 
